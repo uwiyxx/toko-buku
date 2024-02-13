8062732db5e9836b8642a4accf004a8d0e0146c5
uwiyxx <sitinurcahyadewi2803@gmail.com>
2024-02-13 12:11:36 +0700

    first commit

---
+++ layout/footer.php
@@ -0,0 +1,10 @@
+<script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../dist/js/app-style-switcher.js"></script>
+    <!--Wave Effects -->
+    <script src="../dist/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="../dist/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="../dist/js/custom.js"></script>
